88f105d99c985eccf3b98865178791b77f6f1c58
Yichuan Shen <shen.yichuan@gmail.com>
2016-09-26 07:43:38 +0200

    Draw blocks

--- browser/renderer.js
+++ browser/renderer.js
@@ -3,4 +3,16 @@ require('snapsvg')
 exports.render = function(level) {
     let s = Snap('#scene')
     s.clear()
+
+    // Draw background
+
+    s.rect(0, 0, 800, 500).attr({
+        fill: '#121210'
+    })
+
+    // Draw blocks
+
+    level.blocks.forEach(block => {
+        s.rect(...block.position, ...block.size)
+    })
 }
